ceb92dca95e49abfa716df5c11ce90d4b8550066
Austin <austin@Austins-MacBook-Pro-2.local>
2023-06-04 15:57:00 -0400

    updates

--- functions.zsh
+++ functions.zsh
@@ -20,18 +20,12 @@ cluster-fuck() {
   docker volume prune -f
 }
 
-# --------------- LDAP ---------------
-# get my ad auth groups
-myldaps() {
-  curl -s -k --ntlm -u cable\\${NTID} "ldaps://adapps.cable.comcast.com:3269/dc=comcast,dc=com?memberOf?sub?(sAMAccountName=$NTID)"
-}
-
 # --------------- GITHUB ---------------
 # git remote add origin from ghe
 # connect [repo] [user - optional]
 connect () {
   local user="${2:-$NTID}"
-  local url='https://'$GITHUB_TOKEN'@github.comcast.com/'$user'/'$1'.git'
+  local url='https://'$GITHUB_TOKEN'@github.com/'$user'/'$1'.git'
   git remote add origin $url
 }
 
@@ -39,7 +33,7 @@ connect () {
 # connect [repo] [org - optional]
 connect-org () {
   local org="${2:-$GITHUB_ORG}"
-  local url='https://'$GITHUB_TOKEN'@github.comcast.com/'$org'/'$1'.git'
+  local url='https://'$GITHUB_TOKEN'@github.com/'$org'/'$1'.git'
   git remote add origin $url
 }
 
@@ -47,7 +41,7 @@ connect-org () {
 # clone [repo] [optional - user]
 clone () {
   local user="${2:-$NTID}"
-  local url='https://'$GITHUB_TOKEN'@github.comcast.com/'$user'/'$1'.git'
+  local url='https://'$GITHUB_TOKEN'@github.com/'$user'/'$1'.git'
   git clone $url
 }
 
@@ -55,27 +49,27 @@ clone () {
 # clone [repo] [optional - organiztion name]
 clone-org () {
   local org="${2:-$GITHUB_ORG}"
-  local url='https://'$GITHUB_TOKEN'@github.comcast.com/'$org'/'$1'.git'
+  local url='https://'$GITHUB_TOKEN'@github.com/'$org'/'$1'.git'
   git clone $url
 }
 
 # creates a repo on github
 # create-repo [repo] ... this will only create repo in user space
 create-repo() {
-  curl -ss -H "Authorization: token $GITHUB_TOKEN" --data '{"name":"'$1'"}' https://github.comcast.com/api/v3/user/repos >> /dev/null
+  curl -ss -H "Authorization: token $GITHUB_TOKEN" --data '{"name":"'$1'"}' https://github.com/api/v3/user/repos >> /dev/null
 }
 
 # deletes a repo on github
 # be careful  ... only deletes in user space
 delete-repo() {
-  curl -ss -X DELETE -u $NTID:$GITHUB_TOKEN https://github.comcast.com/api/v3/repos/$NTID/$1 
+  curl -ss -X DELETE -u $NTID:$GITHUB_TOKEN https://github.com/api/v3/repos/$NTID/$1 
 }
 
 # clones all the repositories ha ...
 clone-all() {
-  curl -ss -H "Authorization: token $GITHUB_TOKEN" https://github.comcast.com/api/v3/users/$NTID/repos | \
+  curl -ss -H "Authorization: token $GITHUB_TOKEN" https://github.com/api/v3/users/$NTID/repos | \
   grep -w '"name":' | sed 's/.*://' | rev | cut -c 2- | rev | \
-  xargs -L1 -I '$' git clone https://$GITHUB_TOKEN@github.comcast.com/$NTID/$.git
+  xargs -L1 -I '$' git clone https://$GITHUB_TOKEN@github.com/$NTID/$.git
 }
 
 # git push
